bc423533341156a9f4ec7dc446098aa24164ca77
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-06-09 01:58:03 -0300

    change message

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from src.main import app
 def test_read_main(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!!! From Fargate3"}
+    assert response.json() == {"message": "Hello World!!! From Fargate boa noite"}
 
 
 if __name__ == '__main__':
