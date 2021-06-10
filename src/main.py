3a7ea99fba9c5a579204a024d757f1d4efa3cb27
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-06-09 22:20:04 -0300

    random snake

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import random
+
+
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World! Todo mundo Dançando HA HA HA"}
+    return {"message": "Hello World!"}
+
+
+@app.post("/start")
+async def start(data: dict):
+    print(data["game"])
+    return "Ready!"
+
+@app.post("/move")
+async def move(data: dict):
+    choices = ["up", "down", "left", "right"]
+    choice = random.choice(choices)
+    return {"move": choice}
+
+@app.post("/end")
+async def end(data: dict):
+    return "game ended"
+
